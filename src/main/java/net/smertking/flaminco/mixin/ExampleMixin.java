91707905822f5d2f167f73604a978449bd82eb15
Smert King <pokebay77@gmail.com>
2022-04-13 19:21:40 -0400

    Inital Commit

---
+++ src/main/java/net/smertking/flaminco/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.smertking.flaminco.mixin;
+
+import net.smertking.flaminco.Flaminco;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Flaminco.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
